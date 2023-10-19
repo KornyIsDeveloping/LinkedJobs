2c2cfb5d27286fad5de3d3e5671f7392fe4038ce
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2023-10-19 15:44:12 +0300

    style the quote section

--- linkedjobs/src/routes/HomePage.js
+++ linkedjobs/src/routes/HomePage.js
@@ -7,13 +7,13 @@ function HomePage() {
         <>
         <Navbar/>
         {/* generating all of the content from QuotesSection for all the pages */}
-        {/* <QuotesSection
+        <QuotesSection
         cName='quote'
         // quoteImage='https://64.media.tumblr.com/9660e93e840e24acb4576369d487e9a3/tumblr_oj1mn4AcF51qbg3s6o1_500.png'
         quote='"I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear."'
         author='- Nelson Mandela -'
         />
-        <News/> */}
+        {/* <News/> */}
         </>
     )
 }
